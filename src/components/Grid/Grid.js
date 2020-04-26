70964e56945b9269f4c412d1a1c8e3c55c9a059d
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-04-25 23:13:12 -0400

    Added grid view

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -4,20 +4,35 @@ import './style.css';
 const Grid = () =>{
     const [data, setData] = useState([]);
     const [pokeList, setPokeList] = useState([]);
+    const [initialUrl, setUrl] = useState('');
+    const [newData, setNew] = useState([]);
 
     const getData = async () => {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10`);
       const data = await response.json();
       setData(data);
     const pokeList = data.results.map((list) => 
-    <li className="list" style={{ fontSize: '30px' }}key={list.url}>{list.name}</li>);
+    <li className="list" key={list.url}>{list.name}<br />
+    <img src={list.url}/></li>
+    );
         setPokeList(pokeList);
+        const initialUrl = data.results.map((list) => 
+        list.url
+        );
+        setUrl(initialUrl);
+        console.log(initialUrl);
+        
+    const newResponse = await fetch(initialUrl[1]);
+    const newData = await newResponse.json();
+    setNew(newData);
+    console.log(newData);
+
     }
+
+
     useEffect( () =>{
         getData();
       }, [])
-
-console.log(pokeList);
 
   return (
     <div>
